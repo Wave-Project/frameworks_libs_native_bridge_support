f115f441f5a8e72b3db8b7ea1c5c32d4bbee5f61
Bob Badour <bbadour@google.com>
2020-05-20 13:18:00 +0000

    Revert "Add initial files"
    Revert submission 11495882-b145028007_nb_support_switch
    
    Reason for revert: Third-party licensed code not permitted in platform/frameworks per OSPO/Google policy. See http://go/android3p
    Reverted Changes:
    Ie60d2ab11:Add initial files
    Idf808da55:nb_support: switch to new project
    
    Change-Id: Ic3fef2dcffcfa0f0161334eee9a813694989d71d

--- libc/__libc_add_main_thread.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// Guest libc should not insert main thread into thread list, as it was already done by host libc.
-void __libc_add_main_thread() {}
